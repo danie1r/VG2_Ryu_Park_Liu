64abf24a041ca00ab43cb8d0ad2707ca1c9816de
eliu1122 <54511928+eliu1122@users.noreply.github.com>
2023-02-24 21:38:49 -0600

    Done With Resetting Scene

---
+++ VG2_Ryu_Park_Liu/Assets/Reset.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int count = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Raptor"){
+            count++;
+        }
+
+        if (collision.gameObject.name == "Pachycephalasaurus")
+        {
+            count++;
+        }
+
+        if (collision.gameObject.name == "Raptor(Clone)")
+        {
+            count++;
+        }
+
+        if (collision.gameObject.name == "Pachycephalasaurus(Clone)")
+        {
+            count++;
+        }
+
+        if (count >= 10)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
