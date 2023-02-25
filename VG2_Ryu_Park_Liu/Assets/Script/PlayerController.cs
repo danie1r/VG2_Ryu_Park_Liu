aab5d33afddf1d06fa2a3f0e46c35eb3d88fa9ae
danie1r <r.seunghyeondaniel@wustl.edu>
2023-02-24 23:52:41 -0600

    game end

--- VG2_Ryu_Park_Liu/Assets/Script/PlayerController.cs
+++ VG2_Ryu_Park_Liu/Assets/Script/PlayerController.cs
@@ -40,6 +40,12 @@ namespace DinoGame
                         {
                             targetdoor.Interact();
                         }
+
+                        InteractEnd dna = hit.transform.GetComponent<InteractEnd>();
+                        if (dna)
+                        {
+                            dna.Interact();
+                        }
                     }
                 }
             }
