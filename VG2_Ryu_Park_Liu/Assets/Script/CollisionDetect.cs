3e603f71e56cec23eabde8053d96fb96e8fb9b6e
danie1r <r.seunghyeondaniel@wustl.edu>
2023-03-22 19:23:32 -0500

    health update

--- VG2_Ryu_Park_Liu/Assets/Script/CollisionDetect.cs
+++ VG2_Ryu_Park_Liu/Assets/Script/CollisionDetect.cs
@@ -1,22 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class CollisionDetect : MonoBehaviour
+namespace DinoGame
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public class CollisionDetect : MonoBehaviour
     {
-        
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        print("hihihi");
+        public GameObject health;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Enemy")
+            {
+                health.GetComponent<HealthManager>().TakeDamage();
+            }
+        }
     }
 }
+
---
+++ VG2_Ryu_Park_Liu/Assets/Script/HealthManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DinoGame
+{
+    public class HealthManager : MonoBehaviour
+    {
+        public Image damageScreen;
+        public Slider healthBar;
+        public int life = 10;
+        int maxLife;
+        Color alphaColor;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            maxLife = life;
+            healthBar.maxValue = maxLife;
+            alphaColor = damageScreen.color;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            healthBar.value = life;
+
+        }
+
+        public void TakeDamage()
+        {
+            if (life > 0)
+            {
+                life--;
+            }
+            alphaColor.a += .1f;
+            damageScreen.color = alphaColor;
+        }
+
+        public void Heal()
+        {
+            if (life < maxLife)
+            {
+                life++;
+            }
+
+            alphaColor.a -= .1f;
+            damageScreen.color = alphaColor;
+        }
+    }
+}
+
